a21689656cac6987aa7700c087628e3d32834a37
arfazans <arfaz9448@gmail.com>
2025-07-22 23:28:14 +0530

    changes

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -2,9 +2,23 @@ const multer = require('multer');
 const path = require('path');
 
 // Define storage location
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname, '../uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//         const sanitized = file.originalname.replace(/\s+/g, '_');
+//         cb(null, `${Date.now()}-${sanitized}`);
+//     }
+// });
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, './uploads'));
+        const uploadPath = path.join(process.cwd(), 'uploads');
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath);
+        }
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         const sanitized = file.originalname.replace(/\s+/g, '_');
