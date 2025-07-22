12413d8445dd0a0d777c8a1aa066bfd3ba561a9f
arfazans <arfaz9448@gmail.com>
2025-07-22 23:22:45 +0530

    changes

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Define storage location
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads'));
+        cb(null, path.join(__dirname, './uploads'));
     },
     filename: (req, file, cb) => {
         const sanitized = file.originalname.replace(/\s+/g, '_');
